009badceacc1d33ea3f7df809969b37620e13f5c
Maciej Januś <yanuu.why@gmail.com>
2025-07-23 19:57:21 +0200

    fix: error hnadling for send-sms route

--- apps/panel/src/app/api/(token)/(post)/send-sms/route.ts
+++ apps/panel/src/app/api/(token)/(post)/send-sms/route.ts
@@ -5,11 +5,16 @@ import z from "zod";
 
 export const POST = defineRoute(
   async ({ input, req, user }) => {
-    await sendSMS({
-      to: input.to,
-      message: input.message,
-      senderId: user.id,
-    });
+    try {
+      await sendSMS({
+        to: input.to,
+        message: input.message,
+        senderId: user.id,
+      });
+    } catch (error) {
+      console.error(error);
+      return new ApiResponse("Error", 500, "Error sending SMS").toResponse();
+    }
     return new ApiResponse("OK", 200, "OK").toResponse();
   },
   false,
