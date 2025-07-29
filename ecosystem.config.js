57bce7a39d3a49d96ace1b3a27cb2c7352c4a284
Maciej Januś <yanuu.why@gmail.com>
2025-07-29 15:47:37 +0200

    config: add pm2 ecosystem configuration file for easy management

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      namespace: 'serialsms',
+      name: 'modem-worker',
+      script: 'pnpm start',
+      cwd: './apps/modem-worker',
+      watch: ".",
+    }, {
+      namespace: 'serialsms',
+      name: 'web-panel',
+      script: 'pnpm start',
+      cwd: '/apps/panel',
+      watch: ".",
+
+
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
